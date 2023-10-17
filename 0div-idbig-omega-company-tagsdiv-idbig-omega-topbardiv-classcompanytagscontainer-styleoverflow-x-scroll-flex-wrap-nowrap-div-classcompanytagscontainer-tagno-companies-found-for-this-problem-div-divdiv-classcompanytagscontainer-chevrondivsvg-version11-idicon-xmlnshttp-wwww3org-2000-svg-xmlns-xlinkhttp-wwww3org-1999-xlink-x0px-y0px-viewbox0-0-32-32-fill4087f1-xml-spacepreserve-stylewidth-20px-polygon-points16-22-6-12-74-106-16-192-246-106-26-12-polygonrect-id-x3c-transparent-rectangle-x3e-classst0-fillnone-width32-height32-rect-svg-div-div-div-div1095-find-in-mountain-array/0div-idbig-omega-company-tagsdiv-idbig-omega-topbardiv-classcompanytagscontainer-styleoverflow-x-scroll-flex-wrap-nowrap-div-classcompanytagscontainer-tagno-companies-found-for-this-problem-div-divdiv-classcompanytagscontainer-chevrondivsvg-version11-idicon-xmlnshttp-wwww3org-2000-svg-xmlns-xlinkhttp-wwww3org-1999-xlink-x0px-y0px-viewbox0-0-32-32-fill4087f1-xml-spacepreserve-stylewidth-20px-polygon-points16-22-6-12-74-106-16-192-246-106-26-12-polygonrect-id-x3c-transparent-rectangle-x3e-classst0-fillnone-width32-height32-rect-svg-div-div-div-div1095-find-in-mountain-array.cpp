12555a371942736bf6f59b45f4fa5fbedbd5cf18
Rushikesh Jawale <100342436+imrushikesh77@users.noreply.github.com>
2023-10-17 12:04:27 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (54.85%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
@@ -1,51 +1,48 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
 class Solution {
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        int length = mountainArr.length();
-
-        // Find the index of the peak element in the mountain array.
-        int peakIndex = findPeakIndex(1, length - 2, mountainArr);
-
-        // Binary search for the target in the increasing part of the mountain array.
-        int increasingIndex = binarySearch(0, peakIndex, target, mountainArr, false);
-        if (mountainArr.get(increasingIndex) == target) 
-            return increasingIndex; // Target found in the increasing part.
-
-        // Binary search for the target in the decreasing part of the mountain array.
-        int decreasingIndex = binarySearch(peakIndex + 1, length - 1, target, mountainArr, true);
-        if (mountainArr.get(decreasingIndex) == target) 
-            return decreasingIndex; // Target found in the decreasing part.
-
-        return -1;  // Target not found in the mountain array.
-    }
-
-    int findPeakIndex(int low, int high, MountainArray &mountainArr) {
-        while (low != high) {
-            int mid = low + (high - low) / 2;
-            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
-                low = mid + 1; // Move to the right side (increasing slope).
-            } else {
-                high = mid; // Move to the left side (decreasing slope).
+        int i=0;
+        int j=mountainArr.length()-1;
+        int ans = -1;
+        while(i<j){
+            int mid = i + (j-i)/2;
+            if(mountainArr.get(mid)>mountainArr.get(mid+1))j=mid;
+            else i=mid+1;
+             
+        }
+        i=0;
+        int k=j;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(mountainArr.get(mid)<target)i=mid+1;
+            else if(mountainArr.get(mid)>target)j=mid-1;
+            else{
+                ans=mid;
+                break;
             }
         }
-        return low; // Return the index of the peak element.
-    }
-
-    int binarySearch(int low, int high, int target, MountainArray &mountainArr, bool reversed) {
-        while (low != high) {
-            int mid = low + (high - low) / 2;
-            if (reversed) {
-                if (mountainArr.get(mid) > target)
-                    low = mid + 1; // Move to the right side for a decreasing slope.
-                else
-                    high = mid; // Move to the left side for an increasing slope.
-            } else {
-                if (mountainArr.get(mid) < target)
-                    low = mid + 1; // Move to the right side for an increasing slope.
-                else
-                    high = mid; // Move to the left side for a decreasing slope.
+        if(ans<0){
+            j=mountainArr.length()-1;
+            while(k<=j){
+                int mid = k +(j-k)/2;
+                if(mountainArr.get(mid)>target)k=mid+1;
+                else if(mountainArr.get(mid)<target)j=mid-1;
+                else {
+                    ans=mid;
+                    break;
+                }
             }
         }
-        return low; // Return the index where the target should be or would be inserted.
+        return ans;
     }
 };
